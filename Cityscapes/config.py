19857a747ea953ef83dec61b071451fdf7d0638f
V2LLAIN <104286511+V2LLAIN@users.noreply.github.com>
2023-12-12 11:24:59 +0900

    Update config.py

--- Cityscapes/config.py
+++ Cityscapes/config.py
@@ -15,14 +15,14 @@ parser.add_argument('--color_table', default=str(color_table))
 # train.py
 parser.add_argument('--DEVICE', default="cuda")
 parser.add_argument('--save_path', default="./output")
-parser.add_argument('--LEARNING_RATE', type=float, default=5e-4)
+parser.add_argument('--LEARNING_RATE', type=int, default=5e-4)
 parser.add_argument('--EPOCHS', type=int, default=100)
 parser.add_argument('--class_num', type=int, default=19+1)
 parser.add_argument('--smooth', type=float, default=1e-10)
 
 # eval.py
-parser.add_argument('--checkpoint_path', default='/root/Study/ComputerVision/output/best.pth')
-parser.add_argument('--test_save_path', default='./test_result')
+parser.add_argument('--checkpoint_path', default='/root/Study/Computer Vision/output/best.pth')
+parser.add_argument('--eval_save_path', default='./test_result')
 
 args = parser.parse_args()
 
